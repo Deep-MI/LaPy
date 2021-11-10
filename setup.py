12afab63e8fb8d22ca3e54158e9af12b5ef97caf
Ahmed Abdelrahman <ahmedfaisal_95@hotmail.co.uk>
2021-11-10 09:54:22 +0100

    Add direct dependencies in setup.py

--- setup.py
+++ setup.py
@@ -26,11 +26,12 @@ setuptools.setup(
 #        "Operating System :: OS Independent",
     ],
     python_requires='>=3.3',
-    keywords='Laplace, FEM, ShapeDNA, BrainPrint, Triangle Mesh, Tetrahedra Mesh, Geodesics in Heat, Mean Curvature Flow'
+    keywords='Laplace, FEM, ShapeDNA, BrainPrint, Triangle Mesh, Tetrahedra Mesh, Geodesics in Heat, Mean Curvature Flow',
     # This field lists other packages that your project depends on to run.
     # Any package you put here will be installed by pip when your project is
     # installed, so they must be valid existing projects.
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
 #    install_requires=['peppercorn'],  # Optional
+    install_requires=['numpy', 'scipy', 'plotly', 'scikit-sparse'],
 )

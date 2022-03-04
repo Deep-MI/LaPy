87f8536ef5a558147ad8e6ed1b58f689892ea956
Kersten Diers <kersten.diers@dzne.de>
2022-03-04 12:00:23 +0100

    Added color specification

--- lapy/Plot.py
+++ lapy/Plot.py
@@ -64,7 +64,7 @@ def _get_color_levels():
 def _get_colorscale(vmin, vmax):
     # put together a colorscale map depending on the range of v-values
     if vmin > vmax:
-        raise ValueError('incorrect relation between vmin and vmax')  
+        raise ValueError('incorrect relation between vmin and vmax')
     # color definitions
     posstart = 'rgb(255, 0, 0)'
     posstop  = 'rgb(255, 255, 51)'
@@ -108,7 +108,7 @@ def _get_colorscale(vmin, vmax):
 
 
 def _get_colorval(t, colormap):
-    # t must be 0...1 and 
+    # t must be 0...1 and
     # colormap is a list of values and color code strings
     #     (should have entries at least for 0 and 1)
     # we return the interpolated color for this value of t
@@ -152,163 +152,6 @@ def _map_z2color(zval, colormap, zmin, zmax):
         rgb = _get_colorval(t, colormap)
 
     return rgb
-
-
-def plot_tria_mesh(tria, vfunc=None, plot_edges=None, plot_levels=False, tfunc=None,
-                   edge_color='rgb(50,50,50)', tic_color='rgb(50,200,10)', html_output=False,
-                   width=800, height=800, flatshading=False,
-                   xrange=None, yrange=None, zrange=None, 
-                   showcaxis=False, caxis=None):
-    # interesting example codes:
-    # https://plot.ly/~empet/14749/mesh3d-with-intensities-and-flatshading/#/
-    #
-    if type(tria).__name__ != "TriaMesh":
-        raise ValueError('plot_tria_mesh works only on TriaMesh class')
-
-    x, y, z = zip(*tria.v)
-    i, j, k = zip(*tria.t)
-
-    vlines = []
-    if vfunc is None:
-        if tfunc is None:
-            triangles = go.Mesh3d(x=x, y=y, z=z, i=i, j=j, k=k, flatshading=flatshading)
-        elif tfunc.ndim == 1 or (tfunc.ndim == 2 and np.min(tfunc.shape) == 1):
-            # scalar tfunc
-            min_fcol = np.min(tfunc)
-            max_fcol = np.max(tfunc)
-            # special treatment for constant functions
-            if np.abs(min_fcol - max_fcol) < 0.0001:
-                if np.abs(max_fcol) > 0.0001:
-                    min_fcol = -np.abs(min_fcol)
-                    max_fcol = np.abs(max_fcol)
-                else:  # both are zero
-                    min_fcol = -1
-                    max_fcol = 1
-            # if min_fcol >= 0 and max_fcol <= 1:
-            #    min_fcol = 0
-            #    max_fcol = 1
-            # colormap = cm.RdBu
-            colormap = _get_colorscale(min_fcol, max_fcol)
-            facecolor = [_map_z2color(zz, colormap, min_fcol, max_fcol) for zz in tfunc]
-            # for tria colors overwrite flatshading to be true:
-            triangles = go.Mesh3d(x=x, y=y, z=z, i=i, j=j, k=k,
-                                  facecolor=facecolor,
-                                  flatshading=True)
-        elif tfunc.ndim == 2 and np.min(tfunc.shape) == 3:
-            # vector tfunc
-            s = 0.7 * tria.avg_edge_length()
-            centroids = (1.0/3.0) * (tria.v[tria.t[:, 0], :] + tria.v[tria.t[:, 1], :]
-                                     + tria.v[tria.t[:, 2], :])
-            xv = np.column_stack(
-                (centroids[:, 0], centroids[:, 0] + s * tfunc[:, 0],
-                 np.full(tria.t.shape[0], np.nan))).reshape(-1)
-            yv = np.column_stack(
-                (centroids[:, 1], centroids[:, 1] + s * tfunc[:, 1],
-                 np.full(tria.t.shape[0], np.nan))).reshape(-1)
-            zv = np.column_stack(
-                (centroids[:, 2], centroids[:, 2] + s * tfunc[:, 2],
-                 np.full(tria.t.shape[0], np.nan))).reshape(-1)
-            vlines = go.Scatter3d(x=xv, y=yv, z=zv, mode='lines',
-                                  line=dict(color=tic_color, width=2))
-            triangles = go.Mesh3d(x=x, y=y, z=z, i=i, j=j, k=k, flatshading=flatshading)
-        else:
-            raise ValueError('tfunc should be scalar (face color) or 3d for each triangle')
-
-    elif vfunc.ndim == 1 or (vfunc.ndim == 2 and np.min(vfunc.shape) == 1):
-        # scalar vfunc
-        if plot_levels:
-            colorscale = _get_color_levels()
-        else: 
-            colorscale = _get_colorscale(min(vfunc), max(vfunc))
-            
-        triangles = go.Mesh3d(x=x, y=y, z=z, i=i, j=j, k=k,
-                              intensity=vfunc,
-                              colorscale=colorscale,
-                              flatshading=flatshading)
-    elif vfunc.ndim == 2 and np.min(vfunc.shape) == 3:
-        # vector vfunc
-        s = 0.7 * tria.avg_edge_length()
-        xv = np.column_stack(
-            (tria.v[:, 0], tria.v[:, 0] + s * vfunc[:, 0],
-             np.full(tria.v.shape[0], np.nan))).reshape(-1)
-        yv = np.column_stack(
-            (tria.v[:, 1], tria.v[:, 1] + s * vfunc[:, 1],
-             np.full(tria.v.shape[0], np.nan))).reshape(-1)
-        zv = np.column_stack(
-            (tria.v[:, 2], tria.v[:, 2] + s * vfunc[:, 2],
-             np.full(tria.v.shape[0], np.nan))).reshape(-1)
-        vlines = go.Scatter3d(x=xv, y=yv, z=zv, mode='lines', line=dict(color=tic_color, width=2))
-        triangles = go.Mesh3d(x=x, y=y, z=z, i=i, j=j, k=k, flatshading=flatshading)
-    else:
-        raise ValueError('vfunc should be scalar or 3d for each vertex')
-
-    if plot_edges:
-        # 4 points = three edges for each tria, nan to separate triangles
-        # this plots every edge twice (except boundary edges)
-        xe = np.column_stack(
-            (tria.v[tria.t[:, 0], 0], tria.v[tria.t[:, 1], 0], tria.v[tria.t[:, 2], 0],
-             tria.v[tria.t[:, 0], 0], np.full(tria.t.shape[0], np.nan))).reshape(-1)
-        ye = np.column_stack(
-            (tria.v[tria.t[:, 0], 1], tria.v[tria.t[:, 1], 1], tria.v[tria.t[:, 2], 1],
-             tria.v[tria.t[:, 0], 1], np.full(tria.t.shape[0], np.nan))).reshape(-1)
-        ze = np.column_stack(
-            (tria.v[tria.t[:, 0], 2], tria.v[tria.t[:, 1], 2], tria.v[tria.t[:, 2], 2],
-             tria.v[tria.t[:, 0], 2], np.full(tria.t.shape[0], np.nan))).reshape(-1)
-
-        # define the lines to be plotted
-        lines = go.Scatter3d(x=xe,
-                             y=ye,
-                             z=ze,
-                             mode='lines',
-                             line=dict(color=edge_color, width=1.5)
-                             )
-
-        data = [triangles, lines]
-
-    else:
-        data = [triangles]
-
-    if vlines:
-        data.append(vlines)
-
-    # line_marker = dict(color='#0066FF', width=2)
-
-    noaxis = dict(showbackground=False,
-                  showline=False,
-                  zeroline=False,
-                  showgrid=False,
-                  showticklabels=False,
-                  title='')
-
-    layout = go.Layout(
-        width=width,
-        height=height,
-        scene=dict(
-            xaxis=noaxis,
-            yaxis=noaxis,
-            zaxis=noaxis
-            )
-        )
-
-    if xrange is not None:
-        layout.scene.xaxis.update(range=xrange)
-    if yrange is not None:
-        layout.scene.yaxis.update(range=yrange)
-    if zrange is not None:
-        layout.scene.zaxis.update(range=zrange)
-
-    data[0].update(showscale=showcaxis)
-
-    if caxis is not None:
-        data[0].update(cmin=caxis[0])
-        data[0].update(cmax=caxis[1])
-
-    fig = go.Figure(data=data, layout=layout)
-
-    if not html_output:
-        plotly.offline.iplot(fig)
-    else:
-        plotly.offline.plot(fig)
 
 
 def plot_tet_mesh(tetra, vfunc=None, plot_edges=None, plot_levels=False, tfunc=None,
@@ -398,3 +241,179 @@ def plot_tet_mesh(tetra, vfunc=None, plot_edges=None, plot_levels=False, tfunc=N
                    tfunc=tfunc_tria, edge_color=edge_color, html_output=html_output,
                    width=width, height=height, flatshading=flatshading,
                    xrange=xrange, yrange=yrange, zrange=zrange, showcaxis=showcaxis, caxis=caxis)
+
+
+def plot_tria_mesh(tria, vfunc=None, tfunc=None, vcolor=None, tcolor=None,
+                   showcaxis=False, caxis=None, xrange=None, yrange=None,
+                   zrange=None, plot_edges=None, plot_levels=False,
+                   edge_color='rgb(50,50,50)', tic_color='rgb(50,200,10)',
+                   background_color=None, flatshading=False, view=None,
+                   width=800, height=800, camera=None, html_output=False,
+                   export_png=None, scale_png=1.0, no_display=False):
+    # interesting example codes:
+    # https://plot.ly/~empet/14749/mesh3d-with-intensities-and-flatshading/#/
+    #
+    if type(tria).__name__ != "TriaMesh":
+        raise ValueError('plot_tria_mesh works only on TriaMesh class')
+
+    if (vfunc is not None or tfunc is not None) and (vcolor is not None or tcolor is not None):
+        raise ValueError('plot_tria_mesh can only use either vfunc/tfunc or vcolor/tcolor, but not both at the same time')
+
+    if vcolor is not None and tcolor is not None:
+        raise ValueError('plot_tria_mesh can only use either vcolor or tcolor, but not both at the same time')
+
+    x, y, z = zip(*tria.v)
+    i, j, k = zip(*tria.t)
+
+    vlines = []
+    if vfunc is None:
+        if tfunc is None:
+            triangles = go.Mesh3d(x=x, y=y, z=z, i=i, j=j, k=k, flatshading=flatshading, vertexcolor=vcolor, facecolor=tcolor)
+        elif tfunc.ndim == 1 or (tfunc.ndim == 2 and np.min(tfunc.shape) == 1):
+            # scalar tfunc
+            min_fcol = np.min(tfunc)
+            max_fcol = np.max(tfunc)
+            # special treatment for constant functions
+            if np.abs(min_fcol - max_fcol) < 0.0001:
+                if np.abs(max_fcol) > 0.0001:
+                    min_fcol = -np.abs(min_fcol)
+                    max_fcol = np.abs(max_fcol)
+                else:  # both are zero
+                    min_fcol = -1
+                    max_fcol = 1
+            # if min_fcol >= 0 and max_fcol <= 1:
+            #    min_fcol = 0
+            #    max_fcol = 1
+            # colormap = cm.RdBu
+            colormap = _get_colorscale(min_fcol, max_fcol)
+            facecolor = [_map_z2color(zz, colormap, min_fcol, max_fcol) for zz in tfunc]
+            # for tria colors overwrite flatshading to be true:
+            triangles = go.Mesh3d(x=x, y=y, z=z, i=i, j=j, k=k,
+                                  facecolor=facecolor,
+                                  flatshading=True)
+        elif tfunc.ndim == 2 and np.min(tfunc.shape) == 3:
+            # vector tfunc
+            s = 0.7 * tria.avg_edge_length()
+            centroids = (1.0/3.0) * (tria.v[tria.t[:, 0], :] + tria.v[tria.t[:, 1], :]
+                                     + tria.v[tria.t[:, 2], :])
+            xv = np.column_stack(
+                (centroids[:, 0], centroids[:, 0] + s * tfunc[:, 0],
+                 np.full(tria.t.shape[0], np.nan))).reshape(-1)
+            yv = np.column_stack(
+                (centroids[:, 1], centroids[:, 1] + s * tfunc[:, 1],
+                 np.full(tria.t.shape[0], np.nan))).reshape(-1)
+            zv = np.column_stack(
+                (centroids[:, 2], centroids[:, 2] + s * tfunc[:, 2],
+                 np.full(tria.t.shape[0], np.nan))).reshape(-1)
+            vlines = go.Scatter3d(x=xv, y=yv, z=zv, mode='lines',
+                                  line=dict(color=tic_color, width=2))
+            triangles = go.Mesh3d(x=x, y=y, z=z, i=i, j=j, k=k, flatshading=flatshading)
+        else:
+            raise ValueError('tfunc should be scalar (face color) or 3d for each triangle')
+
+    elif vfunc.ndim == 1 or (vfunc.ndim == 2 and np.min(vfunc.shape) == 1):
+        # scalar vfunc
+        if plot_levels:
+            colorscale = _get_color_levels()
+        else:
+            colorscale = _get_colorscale(min(vfunc), max(vfunc))
+
+        triangles = go.Mesh3d(x=x, y=y, z=z, i=i, j=j, k=k,
+                              intensity=vfunc,
+                              colorscale=colorscale,
+                              flatshading=flatshading)
+    elif vfunc.ndim == 2 and np.min(vfunc.shape) == 3:
+        # vector vfunc
+        s = 0.7 * tria.avg_edge_length()
+        xv = np.column_stack(
+            (tria.v[:, 0], tria.v[:, 0] + s * vfunc[:, 0],
+             np.full(tria.v.shape[0], np.nan))).reshape(-1)
+        yv = np.column_stack(
+            (tria.v[:, 1], tria.v[:, 1] + s * vfunc[:, 1],
+             np.full(tria.v.shape[0], np.nan))).reshape(-1)
+        zv = np.column_stack(
+            (tria.v[:, 2], tria.v[:, 2] + s * vfunc[:, 2],
+             np.full(tria.v.shape[0], np.nan))).reshape(-1)
+        vlines = go.Scatter3d(x=xv, y=yv, z=zv, mode='lines', line=dict(color=tic_color, width=2))
+        triangles = go.Mesh3d(x=x, y=y, z=z, i=i, j=j, k=k, flatshading=flatshading)
+    else:
+        raise ValueError('vfunc should be scalar or 3d for each vertex')
+
+    if plot_edges:
+        # 4 points = three edges for each tria, nan to separate triangles
+        # this plots every edge twice (except boundary edges)
+        xe = np.column_stack(
+            (tria.v[tria.t[:, 0], 0], tria.v[tria.t[:, 1], 0], tria.v[tria.t[:, 2], 0],
+             tria.v[tria.t[:, 0], 0], np.full(tria.t.shape[0], np.nan))).reshape(-1)
+        ye = np.column_stack(
+            (tria.v[tria.t[:, 0], 1], tria.v[tria.t[:, 1], 1], tria.v[tria.t[:, 2], 1],
+             tria.v[tria.t[:, 0], 1], np.full(tria.t.shape[0], np.nan))).reshape(-1)
+        ze = np.column_stack(
+            (tria.v[tria.t[:, 0], 2], tria.v[tria.t[:, 1], 2], tria.v[tria.t[:, 2], 2],
+             tria.v[tria.t[:, 0], 2], np.full(tria.t.shape[0], np.nan))).reshape(-1)
+
+        # define the lines to be plotted
+        lines = go.Scatter3d(x=xe,
+                             y=ye,
+                             z=ze,
+                             mode='lines',
+                             line=dict(color=edge_color, width=1.5)
+                             )
+
+        data = [triangles, lines]
+
+    else:
+        data = [triangles]
+
+    if vlines:
+        data.append(vlines)
+
+    # line_marker = dict(color='#0066FF', width=2)
+
+    noaxis = dict(showbackground=False,
+                  showline=False,
+                  zeroline=False,
+                  showgrid=False,
+                  showticklabels=False,
+                  title='')
+
+    layout = go.Layout(
+        width=width,
+        height=height,
+        scene=dict(
+            xaxis=noaxis,
+            yaxis=noaxis,
+            zaxis=noaxis
+            ),
+        plot_bgcolor=background_color,
+        paper_bgcolor=background_color
+        )
+
+    if camera is not None:
+        layout.scene.camera.center.update(camera["center"])
+        layout.scene.camera.eye.update(camera["eye"])
+        layout.scene.camera.up.update(camera["up"])
+
+    if xrange is not None:
+        layout.scene.xaxis.update(range=xrange)
+    if yrange is not None:
+        layout.scene.yaxis.update(range=yrange)
+    if zrange is not None:
+        layout.scene.zaxis.update(range=zrange)
+
+    data[0].update(showscale=showcaxis)
+
+    if caxis is not None:
+        data[0].update(cmin=caxis[0])
+        data[0].update(cmax=caxis[1])
+
+    fig = go.Figure(data=data, layout=layout)
+
+    if no_display is False:
+        if not html_output:
+            plotly.offline.iplot(fig)
+        else:
+            plotly.offline.plot(fig)
+
+    if export_png is not None:
+        fig.write_image(export_png, scale=scale_png)

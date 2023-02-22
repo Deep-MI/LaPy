cbe94b91bd1670770d2a9149f888773857440093
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2023-02-22 13:17:20 +0100

    run isort

--- lapy/__init__.py
+++ lapy/__init__.py
@@ -1,5 +1,5 @@
-from .TriaMesh import TriaMesh  # noqa: F401
-from .TetMesh import TetMesh  # noqa: F401
-from .Solver import Solver  # noqa: F401
 from ._version import __version__  # noqa: F401
+from .Solver import Solver  # noqa: F401
+from .TetMesh import TetMesh  # noqa: F401
+from .TriaMesh import TriaMesh  # noqa: F401
 from .utils._config import sys_info  # noqa: F401

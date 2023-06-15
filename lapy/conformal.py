906792a73c7aa1b30a3e54c7b435f145c134314f
Martin Reuter <reuter@mit.edu>
2023-06-15 15:15:51 +0200

    fix white space

--- lapy/conformal.py
+++ lapy/conformal.py
@@ -202,6 +202,7 @@ def mobius_area_correction_spherical(tria, mapping):
         Vertex coordinates (3d) of the updated spherical conformal parameterization.
     result: array
         Optimal parameters (x) for the Mobius transformation, where
+
         .. math::
             f(z) &= \frac{az+b}{cz+d} \\
                  &= \frac{(x(1)+x(2)*1j)*z+(x(3)+x(4)*1j)}{(x(5)+x(6)*1j)*z+(x(7)+x(8)*1j)}.

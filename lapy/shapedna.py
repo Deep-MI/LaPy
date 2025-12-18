ce5b6745296338c1789d3b90041195480d53e138
Martin Reuter <reuter@mit.edu>
2025-12-19 00:45:05 +0100

    remove optional in docstring

--- lapy/shapedna.py
+++ lapy/shapedna.py
@@ -115,7 +115,7 @@ def compute_shapedna(
         Number of eigenvalues/eigenvectors to compute.
     lump : bool, default=False
         If True, lump the mass matrix (diagonal). See `lapy.Solver` class.
-    aniso : float or tuple of shape (2,), optional
+    aniso : float or tuple of shape (2,) or None, default=None
         Anisotropy for curvature-based anisotropic Laplace. See `lapy.Solver`
         class.
     aniso_smooth : int, default=10
